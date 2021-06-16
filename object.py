ac26d410660d436d2488fc3cb1ef30a56c63d5d5
Yusuf AYTAŞ <79663459+yusufaytasss@users.noreply.github.com>
2021-06-16 13:19:48 +0300

    Add files via upload

---
+++ object.py
@@ -0,0 +1,22 @@
+import turtle
+obj = turtle.Turtle()
+obj.speed(0)
+obj.pencolor("yellow")
+
+win = turtle.Screen()
+win.bgcolor("black")
+win.title("Next codes")
+f = 0
+r = 0
+obj.up()
+obj.goto(0,200)
+obj.down()
+while(1):
+    obj.forward(f)
+    obj.right(r)
+    f+=3
+    r+=1
+    if r >= 200:
+        break
+    obj.hideturtle()
+    turtle.done
